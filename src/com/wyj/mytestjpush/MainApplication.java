0cafbc76fe7d7e17f0b5ebd5e7781d167561034f
Jiesen <13693653307@163.com>
2015-10-15 22:31:12 +0800

    添加了 极光推送的功能

--- src/com/wyj/mytestjpush/MainApplication.java
+++ src/com/wyj/mytestjpush/MainApplication.java
@@ -1,6 +1,8 @@
 package com.wyj.mytestjpush;
 
 import android.app.Application;
+import android.util.Log;
+import cn.jpush.android.api.JPushInterface;
 import com.wyj.mytestjpush.tools.FileCache;
 
 /**
@@ -10,6 +12,8 @@ import com.wyj.mytestjpush.tools.FileCache;
  * Date: 2015/10/14
  */
 public class MainApplication extends Application {
+    private static final String TAG = "JPush";
+
 
     @Override
     public void onCreate() {
@@ -19,6 +23,11 @@ public class MainApplication extends Application {
         FileCache.newInstance(this);
 
         //初始化 极光推送 工具类
+        Log.d(TAG, "[ExampleApplication] onCreate");
+        super.onCreate();
+
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
 
     }
 }
